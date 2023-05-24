2032c5f547ce63185e5bde06e40c761ce98d4131
nogu66 <ezochi66@gmail.com>
2023-05-24 12:30:59 +0900

    Initial Commit

---
+++ SwiftUIApp/SwiftUIAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIAppApp.swift
+//  SwiftUIApp
+//
+//  Created by YutaNoguchi on 2023/05/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
