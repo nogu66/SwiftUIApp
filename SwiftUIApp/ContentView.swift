c0daec2eed7701b8cdadda06afb9aea116dcc5e8
nogu66 <ezochi66@gmail.com>
2023-05-25 21:36:25 +0900

    IgnoreSafeAreaの表示

--- SwiftUIApp/ContentView.swift
+++ SwiftUIApp/ContentView.swift
@@ -7,28 +7,30 @@
 
 import SwiftUI
 
-import MapKit
-
 struct ContentView: View {
-    @State var coordinateRegion = MKCoordinateRegion(
-        // 東京駅の緯度経度
-        center: CLLocationCoordinate2D(
-            latitude: 35.6809591,
-            longitude: 139.7673068
-        ),
-        latitudinalMeters: 1000,
-        longitudinalMeters: 1000
-    )
-    
     var body: some View {
-        // 指定した位置←指定した範囲の地図を表示する
-        Map(coordinateRegion: $coordinateRegion,
-            interactionModes: .all)
+        Color.cyan
+    }
+}
+
+struct IgnoresSafeAreaContentView: View {
+    var body: some View {
+        Color.cyan
+            .ignoresSafeArea()
+    }
+}
+
+struct IgnoresSafeAreaContentView_Previes:
+    PreviewProvider {
+    static var previews: some View {
+        IgnoresSafeAreaContentView()
+            .previewDevice("iPhone 13")
+            
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        ContentView().previewDevice("iPhone 13")
     }
 }
